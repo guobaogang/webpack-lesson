0391d1e0b7c8cef9c471d1bc0cf07057672b60b1
guobgf <guobgf@yonyou.com>
2020-09-21 15:09:11 +0800

    add plugin

--- lesson2/src/index.js
+++ lesson2/src/index.js
@@ -1,16 +1,30 @@
 import './index.less';
-import avatar from './avatar.jpeg';
+// import avatar from './avatar.jpeg';
 import avatar2 from './photo/avatar.jpeg';
 var dom = document.getElementById('app');
 var header = document.createElement('div');
-header.classList.add('header')
-header.append('Header');
-dom.append(header);
+header.classList.add('header');
+header.innerHTML = 'Header';
+dom.appendChild(header);
 
-var img= new Image();
+const arr = [1, 2, 3, 4];
+arr.forEach(item => {
+    let temp = document.createElement('div');
+    temp.innerHTML = item;
+    dom.appendChild(temp);
+});
+
+/* var img = new Image();
 img.src = avatar;
-dom.append(img);
-
-var img= new Image();
+dom.appendChild(img);
+ */
+var img = new Image();
 img.src = avatar2;
-dom.append(img);
+dom.appendChild(img);
+
+
+var timer = 0;
+setInterval(() => {
+    timer++;
+    document.querySelector('.header').innerHTML = timer;
+}, 1000);
--- lesson2/webpack.config.js
+++ lesson2/webpack.config.js
@@ -1,14 +1,28 @@
 var path = require('path');
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var {
+	CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+var webpack = require('webpack');
 
 module.exports = {
-	mode: 'development',
+	mode: 'production',
 	entry: './src/index.js',
+	devtool: 'source-map',
 	output: {
 		filename: 'main.js',
 		path: path.join(__dirname, './dist')
 	},
 	module: {
 		rules: [{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: "babel-loader",
+				/* options: {
+					presets: ['@babel/preset-env']
+				} */
+			},
+			{
 				test: /\.(css|less)$/,
 				use: ['style-loader', 'css-loader', "less-loader"]
 			},
@@ -34,7 +48,7 @@ module.exports = {
 				}]
 			} */
 		]
-	}
+	},
 	// module: {
 	// 	rules: [{
 	// 		test: /\.(css|less)$/,
@@ -53,4 +67,16 @@ module.exports = {
 	// 		]
 	// 	}]
 	// }
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HTMLWebpackPlugin({
+			filename: 'index.html',
+			template: './index.html'
+		}),
+		new webpack.HotModuleReplacementPlugin()
+	],
+	devServer: {
+		hot: true,
+		hotOnly: true
+	}
 }
